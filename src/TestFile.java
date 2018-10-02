94d5e853d0736ccd274a73bfd763d1bcfb7191a0
BT_1N3_28 <rzhao3818@bths.edu>
2018-10-02 14:50:46 -0400

    l

--- src/TestFile.java
+++ src/TestFile.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class TestFile {
+    public static boolean isPalindrome(String word){
+    String testpalin = "";
+    for (int x = word.length()-1; x > 0; x--)
+        testpalin += word.substring()
 }
