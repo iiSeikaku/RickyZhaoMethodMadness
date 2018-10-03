8c25d53786661a540a871e80d105a88f2acff228
iiSeikaku <rzhao3818@bths.edu>
2018-10-03 14:49:25 -0400

    2

--- src/RZhaolib.java
+++ src/RZhaolib.java
@@ -1,9 +1,10 @@
 public class RZhaolib {
     public static boolean isPalindrome(String word) {
         String testpalin = "";
-        for (int x = word.length() - 1; x > 0; x--)
-            testpalin += word.substring(2);
-        return false;
+        for (int x = word.length(); x > 0; x--) {
+            testpalin += word.charAt(x) + testpalin;
+        }
+        testpalin.equals(word);
     }
 }
 
