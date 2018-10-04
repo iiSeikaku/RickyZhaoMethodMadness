899dbf1e2f53316a6101cd0235921410ad4ad663
BT_1N3_27 <rzhao3818@bths.edu>
2018-10-04 14:51:10 -0400

    Adding the code for foobarbaz

--- src/RZhaolib.java
+++ src/RZhaolib.java
@@ -2,10 +2,27 @@ public class RZhaolib {
     public static boolean isPalindrome(String word) {
         String testpalin = "";
         for (int x = word.length(); x > 0; x--) {
-            testpalin += word.charAt(x) + testpalin;
+            testpalin += word.charAt(x-1);
         }
-        testpalin.equals(word);
+        if (testpalin.equals(word)) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
 
 
+public static void fooBarBaz(int num1){
+    String f="foo";
+    String br = "bar";
+    String bz = "baz";
+    for (int i=0; i < num1; i++){
+        if (i%3==0 && i%5!=0)
+            System.out.println(f);
+        else {
+            if (i%5=0 && i%3!=0)
+        }
+    }
+}
