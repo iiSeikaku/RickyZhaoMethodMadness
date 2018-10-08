7e5f7f6d7d5b33ce0413fe73401836a8ab4ce537
Ricky zhao <rzhao3818@bths.edu>
2018-10-07 20:13:34 -0400

    Finished 4 methods, need to add fifth. Need to test methods. Palindrome and sumUpTo should work as I expect, but foobarbaz and cutOut need testing.

--- src/RZhaolib.java
+++ src/RZhaolib.java
@@ -1,51 +1,58 @@
+
 public class RZhaolib {
     public static boolean isPalindrome(String word) {
         String testpalin = "";
         for (int x = word.length(); x > 0; x--) {
-            testpalin += word.charAt(x-1);
+            testpalin += word.charAt(x - 1);
         }
         if (testpalin.equals(word)) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
 
 
+    public static void fooBarBaz(int num1) {
+        String f = "foo";
+        String br = "bar";
+        String ber = "baz";
+        for (int i = 0; i < num1; i++) {
+            if (i % 3 == 0 && i % 5 != 0) {
+                System.out.println(f);
+            }
+            if (i % 3 != 0 && i % 5 == 0) {
+                System.out.println(br);
+            }
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println(ber);
+            } else {
+                System.out.println(i);
+            }
 
-public static void fooBarBaz(int num1) {
-    String f = "foo";
-    String br = "bar";
-    String ber = "baz";
-    for (int i = 0; i < num1; i++) {
-        if (i % 3 == 0 && i % 5 != 0) {
-            System.out.println(f);
         }
-        if (i%3!=0 %% i%5==0){
-            System.out.println(br);
-        }
-        if (i%3==0 && i%5==0){
-            System.out.println(ber)
-        }
-        else
-        {
-            System.out.println(i);
-        }
-
     }
-}
 
-public static String cutOut(String mainstr, String substr){
+    public static String cutOut(String mainstr, String substr) {
         int cutlen = substr.length();
-    int indexOf(substr);
+        int place = mainstr.indexOf(substr);
+        if (place < 0) {
+            return mainstr;
+        } else {
+            if (place == 0) {
+                return mainstr.substring(place + cutlen);
+            } else {
+                return mainstr.substring(0, place) + mainstr.substring(place + cutlen);
+            }
+        }
+    }
+
+    public static int sumUpTo(int num2) {
+        int intsum = 0;
+        for (int p = 0; p <= num2; p++) {
+            intsum += p;
+        }
+        return intsum;
     }
 }
 
-public static int sumUpTo(int num2){
-    int intsum = 0;
-    for (int p = 0; p <= num2; p++){
-        intsum += p;
-    }
-    return intsum;
-}
